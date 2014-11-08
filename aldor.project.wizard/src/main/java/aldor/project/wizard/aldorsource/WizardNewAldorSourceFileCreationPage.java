280be7aa98c5db1ce3968188efd2218b11170cc0
Peter Broadbery <p.broadbery@gmail.com>
2014-11-08 17:51:12 +0000

    whitespace

--- aldor.project.wizard/src/main/java/aldor/project/wizard/aldorsource/WizardNewAldorSourceFileCreationPage.java
+++ aldor.project.wizard/src/main/java/aldor/project/wizard/aldorsource/WizardNewAldorSourceFileCreationPage.java
@@ -7,15 +7,15 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
 
 public class WizardNewAldorSourceFileCreationPage extends WizardNewFileCreationPage {
-    
+
 	public WizardNewAldorSourceFileCreationPage(IStructuredSelection selection) {
         super("Aldor New Source File Wizard", selection);
- 
+
         setTitle("Schema File Wizard");
         setDescription("Create an Aldor Source File");
         setFileExtension("as");
     }
- 
+
     @Override
     protected InputStream getInitialContents() {
         String content = "#include \"incl.as\"\n"; // FIXME - should check project default (if any)
