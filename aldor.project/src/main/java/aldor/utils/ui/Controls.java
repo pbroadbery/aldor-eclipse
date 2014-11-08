e28717b81cd15caf17a95cce97408f195a71b086
Peter Broadbery <p.broadbery@gmail.com>
2014-11-08 17:44:27 +0000

    Controls: Change optionalFieldEditor to store an explicit flag indicating if the value is to take the current default.

--- aldor.project/src/main/java/aldor/utils/ui/Controls.java
+++ aldor.project/src/main/java/aldor/utils/ui/Controls.java
@@ -1,5 +1,6 @@
 package aldor.utils.ui;
 
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
@@ -11,6 +12,8 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Text;
+
+import com.google.common.base.Optional;
 
 import aldor.util.event.EventAdapter;
 import aldor.util.event.EventSource;
@@ -25,21 +28,20 @@ public class Controls {
 	public static class OptionalStringControl {
 		Text text;
 		Button checkbox;
-		String currentValue;
-		EventSource<String> valueSource = new EventSource<>();
+		Optional<String> currentValue;
+		EventSource<Optional<String>> valueSource = new EventSource<>();
 		
-		public EventSource<String> valueSource() {
+		public EventSource<Optional<String>> valueSource() {
 			return valueSource;
 		}
 
 		OptionalStringControl(String currentValue) {
-			valueSource.addListener(new EventAdapter<String>() {
-
+			valueSource.addListener(new EventAdapter<Optional<String>>() {
 				@Override
-				public void onEvent(String event) {
+				public void onEvent(Optional<String> event) {
 					OptionalStringControl.this.currentValue = event;
 				}});
-			valueSource.event(currentValue);
+			valueSource.event(Optional.fromNullable(currentValue));
 		}
 		
 		
@@ -70,7 +72,7 @@ public class Controls {
 				@Override
 				public void modifyText(ModifyEvent e) {
 					if (!checkbox.getSelection())
-						raiseEvent(text.getText());
+						raiseEvent(Optional.fromNullable(text.getText()));
 				}
 			});
 			
@@ -80,10 +82,10 @@ public class Controls {
 				public void widgetSelected(SelectionEvent e) {
 					if (checkbox.getSelection()) {
 						text.setEnabled(false);
-						raiseEvent(null);
+						raiseEvent(Optional.<String>absent());
 					} else {
 						text.setEnabled(true);
-						raiseEvent(text.getText());
+						raiseEvent(Optional.fromNullable(text.getText()));
 					}
 				}
 		
@@ -92,25 +94,26 @@ public class Controls {
 			return composite;
 		}
 		
-		protected void raiseEvent(String text) {
+		protected void raiseEvent(Optional<String> text) {
 			valueSource.event(text);
 		}
 		
-		public void setValue(String value) {
-			if (value == null) {
+		public void setValue(Optional<String> value) {
+			if (!value.isPresent()) {
 				checkbox.setSelection(true);
 				text.setText("");
 				text.setEnabled(false);
 			}
 			else {
-				text.setText(value);
+				text.setText(value.get());
 				text.setEnabled(true);
 				checkbox.setSelection(false);
 			}
 			
 		}
 
-		public String value() {
+		public Optional<String> value() {
+			Assert.isNotNull(currentValue);
 			return this.currentValue;
 		}
 
--- aldor.project/src/main/java/aldor/utils/ui/OptionalFieldEditor.java
+++ aldor.project/src/main/java/aldor/utils/ui/OptionalFieldEditor.java
@@ -1,29 +1,36 @@
 package aldor.utils.ui;
 
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.jface.preference.FieldEditor;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 
 import aldor.utils.ui.Controls.OptionalStringControl;
+
+import com.google.common.base.Optional;
 
 public class OptionalFieldEditor extends FieldEditor {
 
 	OptionalStringControl optionalStringControl = Controls.createOptionalString(null);
 	private String preferenceName;
 	private Control control;
+	private final String tooltip;
+	private final String currentDefaultValue;
 
-	public OptionalFieldEditor(String labelText, String preferenceName, Composite parent) {
+	public OptionalFieldEditor(String preferenceName, String labelText, String labelTooltip, String currentDefaultValue, Composite parent) {
 		super();
 		super.setPreferenceName(preferenceName);
 		super.setLabelText(labelText);
-		
+		this.tooltip = labelTooltip;
 		this.preferenceName = preferenceName;
+		this.currentDefaultValue = currentDefaultValue;
 		this.createControl(parent);
 	}
 
 	@Override
-	protected void adjustForNumColumns(int numColumns) {
+	protected final void adjustForNumColumns(int numColumns) {
 		System.out.println("Number of columns: " + numColumns);
         GridData gd = (GridData) control.getLayoutData();
         gd.horizontalSpan = numColumns - 1;
@@ -32,12 +39,12 @@ public class OptionalFieldEditor extends FieldEditor {
 	}
 
 	@Override
-	protected void doFillIntoGrid(Composite parent, int numColumns) {
-		System.out.println("Filling in grid: " + numColumns);
-		getLabelControl(parent);
-
+	protected final void doFillIntoGrid(Composite parent, int numColumns) {
+		Label label = getLabelControl(parent);
+		if (this.tooltip != null)
+			label.setToolTipText(this.tooltip);
 		control = optionalStringControl.createControl(parent);
-		optionalStringControl.setValue(null);
+		optionalStringControl.setValue(Optional.<String>absent());
 
         GridData gd = new GridData();
         gd.horizontalSpan = numColumns - 1;
@@ -48,18 +55,34 @@ public class OptionalFieldEditor extends FieldEditor {
 
 	@Override
 	protected void doLoad() {
-		String value = this.getPreferenceStore().getString(preferenceName);
-		optionalStringControl.setValue(value);
+		boolean isSet = this.getPreferenceStore().getBoolean(isSetPreferenceName());
+		if (isSet) {
+			String value = this.getPreferenceStore().getString(preferenceName);
+			Assert.isTrue(value.length() > 0);
+			optionalStringControl.setValue(Optional.of(value));
+		}
 	}
 
 	@Override
 	protected void doLoadDefault() {
-		optionalStringControl.setValue(null);
+		optionalStringControl.setValue(Optional.<String>absent());
 	}
 
 	@Override
 	protected void doStore() {
-		this.getPreferenceStore().setValue(preferenceName, optionalStringControl.value());
+		if (optionalStringControl.value() == null) {
+			throw new RuntimeException("Nope");
+		}
+		Optional<String> value = optionalStringControl.value();
+		this.getPreferenceStore().setValue(isSetPreferenceName(), value.isPresent());
+		if (value.isPresent())
+			this.getPreferenceStore().setValue(preferenceName, value.get());
+		else
+			this.getPreferenceStore().setToDefault(preferenceName);
+	}
+
+	private String isSetPreferenceName() {
+		return preferenceName+"IsSet";
 	}
 
 	@Override
