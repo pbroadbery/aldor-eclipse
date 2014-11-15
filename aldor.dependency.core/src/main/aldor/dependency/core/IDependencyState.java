b4bc971f50b182fde588b029e7261944c5e3f080
Peter Broadbery <p.broadbery@gmail.com>
2014-11-15 21:55:29 +0000

    whitespace

--- aldor.dependency.core/src/main/aldor/dependency/core/IDependencyState.java
+++ aldor.dependency.core/src/main/aldor/dependency/core/IDependencyState.java
@@ -1,20 +1,21 @@
 package aldor.dependency.core;
 
 import java.util.Collection;
+
 import com.google.common.base.Function;
 
 public interface IDependencyState<FileObj> {
 
 	void release();
-	
+
 	void aldorFileAdded(FileObj file);
-	
+
 	void aldorFileRemoved(FileObj file);
-	
+
 	void aldorFileChanged(FileObj file);
-	
+
 	Collection<FileObj> needsDependencyUpdate();
-	
+
 	void visitInBuildOrder(Function<FileObj, Boolean> function);
 	void visitInBuildOrder(Function<FileObj, Boolean> function, FileObj start);
 
