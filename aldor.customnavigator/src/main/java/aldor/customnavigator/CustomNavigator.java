a93e4f07d56a593c734083293d2ec390c39316ce
Peter Broadbery <p.broadbery@gmail.com>
2014-11-20 16:52:50 +0000

    Navigator: Tried to get error markers working; not there yet.
    Will abandon for the moment - on the grounds that parsing from .asy might
    be a more useful thing to do.

--- aldor.customnavigator/src/main/java/aldor/customnavigator/CustomNavigator.java
+++ aldor.customnavigator/src/main/java/aldor/customnavigator/CustomNavigator.java
@@ -4,9 +4,12 @@ import org.eclipse.ui.navigator.CommonNavigator;
 
 public class CustomNavigator extends CommonNavigator {
 
+		CustomNavigator() {
+		}
+
 	   @Override
 	    protected Object getInitialInput() {
 	        return new CustomProjectWorkbenchRoot();
 	    }
-	   
+
 }
